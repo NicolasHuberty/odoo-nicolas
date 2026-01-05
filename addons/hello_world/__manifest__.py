292a7577dea26fbc1b44b019ef4a0528f2ad73f5
Nicolas Huberty <huberty.nicolas@hotmail.com>
2026-01-05 22:35:13 +0100

    Add hello_world dummy module

---
+++ addons/hello_world/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hello World',
+    'version': '19.0.1.0.0',
+    'summary': 'A simple test module',
+    'description': 'This is a dummy module to test the Odoo deployment from GitHub.',
+    'author': 'Nicolas',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
